142935965537f6477b126863cf02bb135cb1e289
plutonh1443 <jonghyen51@gmail.com>
2023-09-24 21:08:33 +0900

    Edit multiple_sensor.py

--- Current_Project/mqtt/VL53L1X/multiple_sensor.py
+++ Current_Project/mqtt/VL53L1X/multiple_sensor.py
@@ -8,18 +8,23 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(XSHUT,GPIO.OUT)
 GPIO.output(XSHUT,GPIO.LOW)
 
-tof1 = VL53L1X.VL53L1X(i2c_bus=0, i2c_address=0x29)
+tof1 = VL53L1X.VL53L1X(i2c_bus=0, i2c_address=0x2B)
 tof1.open()
-tof1.change_address(new_address = 0x2B)
+#tof1.change_address(new_address = 0x2B)
 GPIO.setup(XSHUT,GPIO.IN)
-tof2 = VL53L1X.VL53L1X(i2c_bus=0, i2c_address=0x29)
+tof2 = VL53L1X.VL53L1X(i2c_bus=0, i2c_address=0x2B)
 tof2.open()
 # now tof1 has address 0x2B and tof2 has address 0x29
 
-tof1.start_ranging(1)                   # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
-distance_in_mm = tof1.get_distance()    # Grab the range in mm
-tof1.stop_ranging()                     # Stop ranging
+running = True
 
-tof2.start_ranging(1)                   # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
-distance_in_mm = tof2.get_distance()    # Grab the range in mm
-tof2.stop_ranging()                     # Stop ranging
+while running:
+    tof1.start_ranging(1)                   # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
+    distance_in_mm = tof1.get_distance()    # Grab the range in mm
+    tof1.stop_ranging()                     # Stop ranging
+    print(distance_in_mm)
+
+    tof2.start_ranging(1)                   # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
+    distance_in_mm = tof2.get_distance()    # Grab the range in mm
+    tof2.stop_ranging()                     # Stop ranging
+    print(distance_in_mm)
