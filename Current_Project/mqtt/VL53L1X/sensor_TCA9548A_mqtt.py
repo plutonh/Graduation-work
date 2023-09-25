1e17d78f091aa46e345c47d5203cff773a46e741
plutonh1443 <jonghyen51@gmail.com>
2023-09-25 20:45:14 +0900

    Arranging file name

---
+++ Current_Project/mqtt/VL53L1X/sensor_TCA9548A_mqtt.py
@@ -0,0 +1,29 @@
+import time
+import board
+import adafruit_vl53l1x
+import adafruit_tca9548a
+
+# Create I2C bus as normal
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+# Create the TCA9548A object and give it the I2C bus
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+# For each sensor, create it using the TCA9548A channel instead of the I2C object
+vl53_1 = adafruit_vl53l1x.VL53L1X(tca[1])
+vl53_2 = adafruit_vl53l1x.VL53L1X(tca[2])
+
+# OPTIONAL: can set non-default values
+vl53_1.distance_mode = 1
+vl53_2.distance_mode = 1
+vl53_1.timing_budget = 33
+vl53_2.timing_budget = 33
+
+vl53_1.start_ranging()
+vl53_2.start_ranging()
+
+# After initial setup, can just use sensors as normal.
+for i in range(1, 100):
+    print(vl53_1.distance, vl53_2.distance)
+    time.sleep(0.1)
