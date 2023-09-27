2a307d3002beb3e2a9ade2df13436078fdc0b75c
plutonh1443 <jonghyen51@gmail.com>
2023-09-27 16:57:40 +0900

    Edit sensor_TCA9548A_mqtt.py

--- Current_Project/mqtt/Controller/Controller.py
+++ Current_Project/mqtt/Controller/Controller.py
@@ -8,6 +8,7 @@ sensor_state = [0, 0, 0, 0]
 
 global input_lock
 global output_lock
+
 input_lock = 0
 output_lock = 0
 
@@ -95,34 +96,22 @@ def on_message(client, userdata, msg):
     print(publisher_id)
 
     if publisher_id == 0 or publisher_id == 2:
-        if output_lock > 0:
-            output_lock -= 1
-            sensor_state[0] = 0
-            sensor_state[2] = 0
+        if publisher_id == 0:
+            data_0 = float(data)
+            sensor_state[publisher_id] = data_0
         else:
-            if publisher_id == 0:
-                data_0 = float(data)
-                sensor_state[publisher_id] = data_0
-            else:
-                data_2 = float(data)
-                sensor_state[publisher_id] = data_2
-            increase_people(client)
-            input_lock = threshold
+            data_2 = float(data)
+            sensor_state[publisher_id] = data_2
+        increase_people(client)
 
     elif publisher_id == 1 or publisher_id == 3:
-        if input_lock > 0:
-            input_lock -= 1
-            sensor_state[1] = 0
-            sensor_state[3] = 0
+        if publisher_id == 1:
+            data_1 = float(data)
+            sensor_state[publisher_id] = data_1
         else:
-            if publisher_id == 1:
-                data_1 = float(data)
-                sensor_state[publisher_id] = data_1
-            else:
-                data_3 = float(data)
-                sensor_state[publisher_id] = data_3
-            decrease_people(client)
-            output_lock = threshold
+            data_3 = float(data)
+            sensor_state[publisher_id] = data_3
+        decrease_people(client)
     
     elif publisher_id == 4: # 2 -> 4 for additional sensors
         data = int(data)
@@ -137,3 +126,145 @@ mqtt_client.connect('test.mosquitto.org')
 mqtt_client.subscribe("embed/control", qos=1)
 
 mqtt_client.loop_forever()
+
+
+
+# import paho.mqtt.client as paho
+# import time
+
+# available_people = 0
+# guideline = 20
+# threshold = 1
+# sensor_state = [0, 0, 0, 0]
+
+# global input_lock
+# global output_lock
+# input_lock = 0
+# output_lock = 0
+
+# ##FIXME: Do not close the gate when there is a person!!
+
+
+# def handle_change(mqtt_client, change):
+#     global available_people
+#     if change == 1:
+#         if available_people == 0:
+#             # Open the gate
+#             mqtt_client.publish('embed/motor', "1")
+#             print("send 1 to motor, open")
+#         available_people += 1
+#     elif change < 0:
+#         prev_num_people = available_people
+#         available_people += change
+
+#         if prev_num_people > 0 and available_people <= 0:
+#             # Close the gate
+#             mqtt_client.publish('embed/motor', "0")
+#             print("send 0 to motor, close")
+    
+#     mqtt_client.publish('embed/web', str(available_people))
+#     print("send %d to the website" % available_people)
+
+# def increase_people(mqtt_client):
+#     if sensor_state[0] != 0 and sensor_state[2] != 0:
+#         # The payload between two sensors is 20 cm 
+#         # The detected length = 20 cm - sensor_state[0] - sensor_state[1]
+#         # Let's conclude there are two people when the length > 12cm 
+#         # sensor_state[0] + sensor_state[1] < 8 cm
+#         if sensor_state[0] + sensor_state[2] < guideline:
+#             # Two people
+#             print("Two people IN")
+#             handle_change(mqtt_client, +2)
+#         else:
+#             # One people
+#             print("One people IN")
+#             handle_change(mqtt_client, +1)
+
+#         sensor_state[0] = 0
+#         sensor_state[1] = 0
+#         sensor_state[2] = 0
+#         sensor_state[3] = 0
+
+#         print("increase")
+
+# def decrease_people(mqtt_client):
+#     if sensor_state[1] != 0 and sensor_state[3] != 0:
+#         # The payload between two sensors is 20 cm 
+#         # The detected length = 20 cm - sensor_state[0] - sensor_state[1]
+#         # Let's conclude there are two people when the length > 12cm 
+#         # sensor_state[0] + sensor_state[1] < 8 cm
+#         if sensor_state[1] + sensor_state[3] < guideline:
+#             # Two people
+#             print("Two people OUT")
+#             handle_change(mqtt_client, -2)
+#         else:
+#             # One people
+#             print("One people OUT")
+#             handle_change(mqtt_client, -1)
+
+#         sensor_state[0] = 0
+#         sensor_state[1] = 0
+#         sensor_state[2] = 0
+#         sensor_state[3] = 0
+
+#         print("decrease")  
+
+# def on_subscribe(client, userdata, mid, granted_qos):
+#     print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+# def on_publish(client,userdata,result):             #create function for callback
+#     print("data published \n")
+#     pass
+
+# def on_message(client, userdata, msg):
+#     global input_lock
+#     global output_lock
+
+#     publisher_id, data = msg.payload.decode("utf-8").split(' ')
+#     publisher_id = int(publisher_id)
+
+#     print(publisher_id)
+
+#     if publisher_id == 0 or publisher_id == 2:
+#         if output_lock > 0:
+#             output_lock -= 1
+#             sensor_state[0] = 0
+#             sensor_state[2] = 0
+#         else:
+#             if publisher_id == 0:
+#                 data_0 = float(data)
+#                 sensor_state[publisher_id] = data_0
+#             else:
+#                 data_2 = float(data)
+#                 sensor_state[publisher_id] = data_2
+#             increase_people(client)
+#             input_lock = threshold
+
+#     elif publisher_id == 1 or publisher_id == 3:
+#         if input_lock > 0:
+#             input_lock -= 1
+#             sensor_state[1] = 0
+#             sensor_state[3] = 0
+#         else:
+#             if publisher_id == 1:
+#                 data_1 = float(data)
+#                 sensor_state[publisher_id] = data_1
+#             else:
+#                 data_3 = float(data)
+#                 sensor_state[publisher_id] = data_3
+#             decrease_people(client)
+#             output_lock = threshold
+    
+#     elif publisher_id == 4: # 2 -> 4 for additional sensors
+#         data = int(data)
+#         print("%s from the web" % data)
+#         handle_change(client, data)
+
+# mqtt_client = paho.Client()
+# mqtt_client.on_subscribe = on_subscribe
+# mqtt_client.on_publish = on_publish
+# mqtt_client.on_message = on_message
+# mqtt_client.connect('test.mosquitto.org')
+# mqtt_client.subscribe("embed/control", qos=1)
+
+# mqtt_client.loop_forever()
--- Current_Project/mqtt/VL53L1X/sensor0_TCA9548A_mqtt.py
+++ Current_Project/mqtt/VL53L1X/sensor0_TCA9548A_mqtt.py
@@ -4,6 +4,12 @@ import adafruit_vl53l1x
 import adafruit_tca9548a
 import paho.mqtt.client as paho
 
+global count_blocked_1
+global count_blocked_2
+
+count_blocked_1 = 0
+count_blocked_2 = 0
+
 broker="test.mosquitto.org"
 
 def on_publish(client,userdata,result):             #create function for callback
@@ -45,46 +51,127 @@ threshold = 2
 
 def main():
     count_blocked_1 = 0
-    count_unblocked_1 = 0
     count_blocked_2 = 0
-    count_unblocked_2 = 0
 
     while True:
         if vl53_1.data_ready:
-            if vl53_1.distance is not None and  vl53_1.distance < guideline:
+            if vl53_1.distance is not None and vl53_1.distance < guideline:
                 count_blocked_1 += 1
-                count_unblocked_1 = 0
-                if count_blocked_1 == threshold:
-                    sensor_client.publish("embed/control", "0 " + str(vl53_1.distance))
-                    print("Send %s" % str(vl53_1.distance))
-                    
-            elif vl53_1.distance is not None and count_blocked_1 != 0:
-                count_unblocked_1 += 1
-                if count_unblocked_1 == 2:
-                    count_blocked_1 = 0
-                    count_unblocked_1 = 0
-
 
             vl53_1.clear_interrupt()
             time.sleep(0.025)
 
         if vl53_2.data_ready:
-            if vl53_2.distance is not None and  vl53_2.distance < guideline:
+            if vl53_2.distance is not None and vl53_2.distance < guideline:
                 count_blocked_2 += 1
-                count_unblocked_2 = 0
-                if count_blocked_2 == threshold:
-                    sensor_client.publish("embed/control", "1 " + str(vl53_2.distance))
-                    print("Send %s" % str(vl53_2.distance))
-                    
-            elif vl53_2.distance is not None and count_blocked_2 != 0:
-                count_unblocked_2 += 1
-                if count_unblocked_2 == 2:
-                    count_blocked_2 = 0
-                    count_unblocked_2 = 0
-
 
             vl53_2.clear_interrupt()
             time.sleep(0.025)
 
+        if count_blocked_1 > count_blocked_2: # Input
+            sensor_client.publish("embed/control", "0 " + str(vl53_1.distance))
+            print("Send %s" % str(vl53_1.distance))
+
+        else: # Output
+            sensor_client.publish("embed/control", "1 " + str(vl53_2.distance))
+            print("Send %s" % str(vl53_2.distance))
+        
+        count_blocked_1 = 0
+        count_blocked_2 = 0
+        time.sleep(1)
+
 if (__name__ == '__main__'):
     main()
+
+
+
+# import time
+# import board
+# import adafruit_vl53l1x
+# import adafruit_tca9548a
+# import paho.mqtt.client as paho
+
+# broker="test.mosquitto.org"
+
+# def on_publish(client,userdata,result):             #create function for callback
+#     print("data published \n")
+#     pass
+
+# sensor_client= paho.Client("sensor0")                           #create client object
+# sensor_client.on_publish = on_publish                          #assign function to callback
+# sensor_client.connect(broker)
+
+# # Create I2C bus as normal
+# i2c = board.I2C()  # uses board.SCL and board.SDA
+# # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+# # Create the TCA9548A object and give it the I2C bus
+# tca = adafruit_tca9548a.TCA9548A(i2c)
+
+# # For each sensor, create it using the TCA9548A channel instead of the I2C object
+# vl53_1 = adafruit_vl53l1x.VL53L1X(tca[1])
+# vl53_2 = adafruit_vl53l1x.VL53L1X(tca[2])
+
+# # OPTIONAL: can set non-default values
+# vl53_1.distance_mode = 1
+# vl53_2.distance_mode = 1
+# vl53_1.timing_budget = 33
+# vl53_2.timing_budget = 33
+
+# vl53_1.start_ranging()
+# vl53_2.start_ranging()
+
+# guideline = 15
+# max_difference = 30
+# threshold = 2
+
+# # After initial setup, can just use sensors as normal.
+# # for i in range(1, 100):
+# #     print(vl53_1.distance, vl53_2.distance)
+# #     time.sleep(0.1)
+
+# def main():
+#     count_blocked_1 = 0
+#     count_unblocked_1 = 0
+#     count_blocked_2 = 0
+#     count_unblocked_2 = 0
+
+#     while True:
+#         if vl53_1.data_ready:
+#             if vl53_1.distance is not None and  vl53_1.distance < guideline:
+#                 count_blocked_1 += 1
+#                 count_unblocked_1 = 0
+#                 if count_blocked_1 == threshold:
+#                     sensor_client.publish("embed/control", "0 " + str(vl53_1.distance))
+#                     print("Send %s" % str(vl53_1.distance))
+                    
+#             elif vl53_1.distance is not None and count_blocked_1 != 0:
+#                 count_unblocked_1 += 1
+#                 if count_unblocked_1 == 2:
+#                     count_blocked_1 = 0
+#                     count_unblocked_1 = 0
+
+
+#             vl53_1.clear_interrupt()
+#             time.sleep(0.025)
+
+#         if vl53_2.data_ready:
+#             if vl53_2.distance is not None and  vl53_2.distance < guideline:
+#                 count_blocked_2 += 1
+#                 count_unblocked_2 = 0
+#                 if count_blocked_2 == threshold:
+#                     sensor_client.publish("embed/control", "1 " + str(vl53_2.distance))
+#                     print("Send %s" % str(vl53_2.distance))
+                    
+#             elif vl53_2.distance is not None and count_blocked_2 != 0:
+#                 count_unblocked_2 += 1
+#                 if count_unblocked_2 == 2:
+#                     count_blocked_2 = 0
+#                     count_unblocked_2 = 0
+
+
+#             vl53_2.clear_interrupt()
+#             time.sleep(0.025)
+
+# if (__name__ == '__main__'):
+#     main()
