ba3d9d9c87229ab090138b299155000aecca58ab
plutonh1443 <jonghyen51@gmail.com>
2023-09-27 17:42:51 +0900

    Edit sensor1_TCA9548A_mqtt.py

--- Current_Project/mqtt/VL53L1X/sensor1_TCA9548A_mqtt.py
+++ Current_Project/mqtt/VL53L1X/sensor1_TCA9548A_mqtt.py
@@ -74,7 +74,7 @@ def main():
             print("2, ", count_blocked_1)
             count_blocked_1 = 0
             count_blocked_2 = 0
-            time.sleep(0.3)
+            time.sleep(0.1)
 
         elif (count_blocked_2 > count_blocked_1) and (count_blocked_2 > threshold): # Output
             sensor_client.publish("embed/control", "3 " + str(vl53_2.distance))
@@ -82,7 +82,7 @@ def main():
             print("3, ", count_blocked_2)
             count_blocked_1 = 0
             count_blocked_2 = 0
-            time.sleep(0.3)
+            time.sleep(0.1)
 
 if (__name__ == '__main__'):
     main()
