b67b058ddd4a90190edffbff525be05b293b61b2
plutonh1443 <jonghyen51@gmail.com>
2023-10-02 21:52:28 +0900

    Edit sensor parameter

--- mqtt/VL53L1X/sensor1_TCA9548A_mqtt.py
+++ mqtt/VL53L1X/sensor1_TCA9548A_mqtt.py
@@ -72,16 +72,18 @@ def main():
             sensor_client.publish("embed/control", "2 " + str(vl53_1.distance))
             print("Send %s" % str(vl53_1.distance))
             print("2, ", count_blocked_1)
+            count_blocked_1 = 0
+            count_blocked_2 = 0
 
-        if (count_blocked_2 > count_blocked_1) and (count_blocked_2 > threshold): # Output
+        elif (count_blocked_2 > count_blocked_1) and (count_blocked_2 > threshold): # Output
             sensor_client.publish("embed/control", "3 " + str(vl53_2.distance))
             print("Send %s" % str(vl53_2.distance))
             print("3, ", count_blocked_2)
+            count_blocked_1 = 0
+            count_blocked_2 = 0
+            
+        time.sleep(0.5)
         
-        count_blocked_1 = 0
-        count_blocked_2 = 0
-        time.sleep(0.3)
-
         vl53_1.clear_interrupt()
         vl53_2.clear_interrupt()
         time.sleep(0.025)
