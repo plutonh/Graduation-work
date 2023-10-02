db9e5a956266f914b25abb04c624c55eb1c48795
plutonh1443 <jonghyen51@gmail.com>
2023-10-02 21:20:03 +0900

    Edit stepperMotor.py

--- mqtt/StepperMotor/stepperMotor.py
+++ mqtt/StepperMotor/stepperMotor.py
@@ -1,3 +1,4 @@
+import paho.mqtt.client as paho
 import time
 import RPi.GPIO as GPIO
 
@@ -12,22 +13,26 @@ for pin in StepPins:
 StepCount = 4
 TargetAngle = 526
 
-StepCounter = 0
-StepAccum = 0
-Direction = 0
+Seq_1 = [[1,0,0,0],
+         [0,1,0,0],
+         [0,0,1,0],
+         [0,0,0,1]]
 
-Seq_1 = [[0,0,0,1],
+Seq_2 = [[0,0,0,1],
          [0,0,1,0],
          [0,1,0,0],
          [1,0,0,0]]
 
-Seq_2 = [[1,0,0,0],
-         [0,1,0,0],
-         [0,0,1,0],
-         [0,0,0,1]]
-try:
-    
-    if(Direction == 1):
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+    StepCounter = 0
+    StepAccum = 0
+
+    if(int(msg.payload) == 1):
         while 1:
             for pin in range(0, 4):
                 xpin = StepPins[pin]
@@ -49,7 +54,6 @@ try:
                 break
             else:
                 time.sleep(0.002)
-
     else:
         while 1:
             for pin in range(0, 4):
@@ -73,5 +77,10 @@ try:
             else:
                 time.sleep(0.002)
 
-except KeyboardInterrupt:
-    GPIO.clenup()
+client = paho.Client()
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.connect('test.mosquitto.org')
+client.subscribe("embed/motor", qos=1)
+
+client.loop_forever()
